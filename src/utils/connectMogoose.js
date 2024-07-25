087c8372a979487fb12dce391fc917d0c7eb26be
Santiagomadaw <santiagoma.daw@gmail.com>
2024-07-25 13:54:23 +0200

    script inicio BBDD

---
+++ src/utils/connectMogoose.js
@@ -0,0 +1,16 @@
+/* This code snippet is setting up a connection to a MongoDB database using Mongoose, which is an
+Object Data Modeling (ODM) library for MongoDB and Node.js. Here's a breakdown of what each part of
+the code is doing: */
+
+const mongoose = require('mongoose');
+// en caso de evento error
+mongoose.connection.on('error', err => {
+    console.log('Error de conexión ', err);
+});
+// en caso de evento open
+mongoose.connection.once('open', () => {
+    console.log('Conectado a MongoDB en', mongoose.connection.name);
+});
+console.log(process.env.JWT_EXPIRE);
+mongoose.connect('mongodb://localhost:27017/blogrest');
+module.exports = mongoose.connection;
