cd2f684d56692d702bbf25a91be7eaa78fa59a7a
Santiagomadaw <santiagoma.daw@gmail.com>
2024-07-27 12:17:47 +0200

    modificacion script, 4 users 14 articulos y encriptacion de password

--- src/initDB/initDB.js
+++ src/initDB/initDB.js
@@ -11,6 +11,7 @@ const readline = require('node:readline');
 
 const User = require('../models/User');
 const data = require('./startedDB.json');
+const users = require('./startedUsers.json');
 const Ad = require('../models/Ad');
 
 function secureQuestion(text) {
@@ -30,39 +31,22 @@ function secureQuestion(text) {
 async function initUsers() {
     const del = await User.deleteMany();
     console.log(`Se han borrado ${del.deletedCount} users.`);
+
+
     
-    const inserted = await User.insertMany([
-    {
-    username: 'admin',
-    name: 'Admin',
-    lastname: 'Admin',
-    email: 'admin@example.com',
-    password: `123123`,
-    role: 'admin',
-    birthdate: new Date('1990-01-01'),
-    address: 'Calle administrativa 123',
-    creditCard: '1234-5678-9012-3456'
-    },
-    {
-    username: 'user',
-    name: 'User',
-    lastname: 'User',
-    email: 'user@example.com',
-    password: `123123`,
-    role: 'user',
-    birthdate: new Date('1995-01-01'),
-    address: 'Calle usuario 123',
-    creditCard: '9876-5432-1098-7654'
+    
+    for (const user of users) {
+        const insertedUser = await User.create(user);
+        console.log(insertedUser);
     }
-    ]);
-    console.log(`Se han creado ${inserted.length} users.`);
-    }
+}
+
 async function initAd() {
 
     const del = await Ad.deleteMany();
     console.log(`Se han borrado ${del.deletedCount} anuncios.`);
     const users = await User.find();
-    console.log(users)
+    console.log(users);
     // Añade anuncios
     let userIndex = 0;
     const updatedData = data.map(item => {
