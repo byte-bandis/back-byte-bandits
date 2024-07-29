baa6c0a7b9b5ffb83d43456d239e23d231d96c89
Jorge Q. <jqb94@hotmail.com>
2024-07-29 23:49:56 +0200

    Add file upload to POST ads endpoint

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const upload = require('../utils/publicUploadConfigure');
 
 const {
 getAds,
@@ -13,7 +14,7 @@ const { authenticate } = require('../middleware/auth');
 
 router.get('/', getAds);
 router.get('/:id', getAd);
-router.post('/', authenticate, createAd);
+router.post('/', authenticate, upload.single('photo'), createAd);
 router.put('/:id', authenticate, updateAd);
 router.delete('/:id', authenticate, deleteAd);
 router.post('/:id/reserve', authenticate, reserveAd);
