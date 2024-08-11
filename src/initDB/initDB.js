19c215ef26c2dad6ec8547b9087b77f72abfc972
Santiagomadaw <santiagoma.daw@gmail.com>
2024-08-11 19:31:33 +0200

    initdb back to the future

--- src/initDB/initDB.js
+++ src/initDB/initDB.js
@@ -5,12 +5,13 @@
 'use strict';
 
 require('dotenv').config();
-const connection = require('../utils/connectMogoose.js');
+const connection = require('../utils/connectMogoose');
 
 const readline = require('node:readline');
 
 const User = require('../models/User');
 const data = require('./startedDB.json');
+const users = require('./startedUsers.json');
 const Ad = require('../models/Ad');
 
 function secureQuestion(text) {
@@ -30,44 +31,23 @@ function secureQuestion(text) {
 async function initUsers() {
     const del = await User.deleteMany();
     console.log(`Se han borrado ${del.deletedCount} users.`);
+
+
     
-    const inserted = await User.insertMany([
-    {
-    username: 'admin',
-    name: 'Admin',
-    lastname: 'Admin',
-    email: 'admin@example.com',
-    password: `123123`,
-    role: 'admin',
-    birthdate: new Date('1990-01-01'),
-    address: 'Calle administrativa 123',
-    creditCard: '1234-5678-9012-3456'
-    },
-    {
-    username: 'user',
-    name: 'User',
-    lastname: 'User',
-    email: 'user@example.com',
-    password: `123123`,
-    role: 'user',
-    birthdate: new Date('1995-01-01'),
-    address: 'Calle usuario 123',
-    creditCard: '9876-5432-1098-7654'
+    
+    for (const user of users) {
+        const insertedUser = await User.create(user);
+        console.log(insertedUser);
     }
-    ]);
-    console.log(`Se han creado ${inserted.length} users.`);
-    }
+}
+
 async function initAd() {
-    /* The line `const del = await Ad.deleteMany();` is deleting all documents from the Ad collection
-    in the database. */
+
     const del = await Ad.deleteMany();
     console.log(`Se han borrado ${del.deletedCount} anuncios.`);
     const users = await User.find();
-    console.log(users)
+    console.log(users);
     // Añade anuncios
-
-    /* The line `const insertAds = await Ad.insertMany(data);` is inserting multiple documents into the
-    Ad collection in the database. */
     let userIndex = 0;
     const updatedData = data.map(item => {
         item.user = users[userIndex]._id;
@@ -79,10 +59,6 @@ async function initAd() {
 }
 
 
-
-/**
- * The main function asynchronously initializes an ad and then closes the connection.
- */
 async function main() {
     await new Promise(resolve => connection.once('open', resolve));
 
@@ -90,12 +66,6 @@ async function main() {
 
     if (!deleleAll) {
 
-        /* The line `process.exit();` is a Node.js method that terminates the current process with an exit code
-        of 0. This means that it will stop the execution of the program and exit the Node.js process. In
-        this specific context within the code snippet provided, if the user does not confirm the deletion of
-        all content in the database by entering 'si', the program will exit without further execution. 
-        you can use it directly since process is a global object in Node.js and is available in all JavaScript 
-        files running in the Node.js environment.*/
         process.exit();
 
     } else {
@@ -105,7 +75,4 @@ async function main() {
 
     }
 }
-
-/* The line `main().catch((err) => console.log(`Hubo un error `));` is handling any errors that
-occur during the execution of the `main()` function.*/
 main().catch((err) => console.log(`Hubo un error ${err}`));
