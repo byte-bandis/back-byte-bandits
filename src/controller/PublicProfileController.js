0f9070558c10ace8d044bc8515485278a8420d4d
Adrián S <adriansotes@gmail.com>
2024-08-22 11:01:08 +0200

    feat: delete mehtod deletes photos after deleting profiles and img route includes the name of the user

--- src/controller/PublicProfileController.js
+++ src/controller/PublicProfileController.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const PublicProfile = require("../models/PublicProfile");
 const { tryCatch } = require("../utils/tryCatch");
+const fs = require("fs").promises;
+const path = require("node:path");
 
 exports.createPublicProfile = tryCatch(async (req, res) => {
   const authHeader = req.headers.authorization;
@@ -188,8 +190,8 @@ exports.deletePublicProfile = tryCatch(async (req, res) => {
   }
 
   const token = authHeader.split(" ")[1];
-
   let decodedToken;
+
   try {
     decodedToken = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
@@ -199,7 +201,6 @@ exports.deletePublicProfile = tryCatch(async (req, res) => {
   }
 
   const requesterId = decodedToken.user._id;
-
   const retrievedUser = await User.findOne({ username });
 
   if (!retrievedUser) {
@@ -222,6 +223,42 @@ exports.deletePublicProfile = tryCatch(async (req, res) => {
     return res.status(404).json({
       message: `User ${username} doesn't have a public profile`,
     });
+  }
+
+  try {
+    if (
+      retrievedProfile.userPhoto &&
+      retrievedProfile.userPhoto !== "UserTemplate.jpg"
+    ) {
+      const userPhotoPath = path.join(
+        __dirname,
+        "..",
+        "public",
+        "images",
+        "profiles",
+        retrievedProfile.userPhoto
+      );
+      await fs.unlink(userPhotoPath);
+      console.log(`${username}'s image deleted successfully now!`);
+    }
+
+    if (
+      retrievedProfile.headerPhoto &&
+      retrievedProfile.headerPhoto !== "UserHeader.jpg"
+    ) {
+      const headerPhotoPath = path.join(
+        __dirname,
+        "..",
+        "public",
+        "images",
+        "profiles",
+        retrievedProfile.headerPhoto
+      );
+      await fs.unlink(headerPhotoPath);
+      console.log(`${username}'s header image deleted successfully now!`);
+    }
+  } catch (error) {
+    console.error("Error deleting image files:", error.message);
   }
 
   await PublicProfile.deleteOne({ user });
