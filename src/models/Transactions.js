67a4f351bdc28a48214a30ebb6dc5e0c9ce0cc7f
Santiagomadaw <santiagoma.daw@gmail.com>
2024-08-28 12:53:58 +0200

    modelo de transacciones

---
+++ src/models/Transactions.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const TransactionsSchema = new mongoose.Schema({
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please add a user']
+  },
+  buyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please add a user']
+  },
+  ad: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ad',
+    required: [true, 'Please add an anuncio']
+  },
+  state : {
+    type: String,
+    enum: ['NoTrade', 'Ordered', 'Sold', 'Cancelled'],
+    default: 'NoTrade'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Like', LikeSchema);
