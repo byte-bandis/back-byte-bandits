61257e26ffa2cfab5ca5c54bb7ceae487786feb8
Adrián S <adriansotes@gmail.com>
2024-09-09 09:37:31 +0200

    feat: nodemailer creates the token to restore password and sends the link with token by email

--- src/controller/myPersonalData/passwordController.js
+++ src/controller/myPersonalData/passwordController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 const User = require("../../models/User");
 const { tryCatch } = require("../../utils/tryCatch");
 
