c5741cab02a56d799aceecfe2764f5224dd6f84f
Santiagomadaw <santiagoma.daw@gmail.com>
2024-09-11 17:38:26 +0200

    finalizado microservicio correo

--- src/controller/NodemailerController.js
+++ src/controller/NodemailerController.js
@@ -1,17 +1,69 @@
-const cote = require('cote');
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { Requester } = require('cote');
 //const { __ } = require("i18n");
 const { tryCatch } = require("../utils/tryCatch");
 const crypto = require("crypto");
+const { transporter } = require("../utils/nodeMailer");
 const {
   welcomeTemplate,
   resetPasswordTemplate,
 } = require("../utils/mainTemplates");
 
-const emailRequester = new cote.Requester({ name: 'email-requester', key: 'email' });
-emailRequester.send({ type: 'create-mail', email: 'test@example.com', subject: 'Test Email' }, (res) => {
-  console.log('Respuesta del Responder:', res);
+exports.sendEmail = tryCatch(async (req, res) => {
+  const requester = new Requester({ name: 'nodemailer', key: 'email' });
+  const { email, userName, type } = req.body;
+  let html;
+  let subject;
+
+  const hostURI =
+    process.env.NODE_ENV !== "production"
+      ? `${process.env.DEV_HOST_URI}`
+      : `${process.env.HOST_URI}`;
+
+  switch (type) {
+    case "welcome":
+      html = welcomeTemplate(userName);
+      subject = res.__("Welcome_to_the_platform") + " " + userName;
+      break;
+
+    case "resetPassword":
+      if (!email) {
+        return res.status(400).json({
+          state: "error",
+          message: res.__("Please_enter_a_valid_email"),
+        });
+      }
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({
+          state: "error",
+          message: res.__("email_not_found", { email }),
+        });
+      }
+
+      const token = crypto.randomBytes(20).toString("hex");
+
+      user.resetPasswordToken = token;
+      user.resetPasswordExpires = Date.now() + 3600000;
+
+      await user.save();
+
+      const resetLink = `${hostURI}/reset-password/${token}`;
+      subject = res.__("Reset_your_password") + " " + user.username;
+      html = resetPasswordTemplate(user.username, resetLink, res);
+      break;
+
+    default:
+      return res.status(400).json({ message: res.__("Invalid_email_type") });
+  }
+  
+  requester.send({ type: 'create-mail', email, subject, html });
+  res.status(200).json({
+    state: "success",
+    message: res.__("Email_sent_successfully"),
+  });
 });
 
 exports.resetPassword = tryCatch(async (req, res) => {
@@ -56,68 +108,5 @@ exports.resetPassword = tryCatch(async (req, res) => {
   res.status(200).json({
     state: "success",
     message: res.__("password_has_been_reset"),
-  });
-});
-
-exports.sendEmail = tryCatch(async (req, res) => {
-  const { email, userName, type } = req.body;
-  let html;
-  let subject;
-
-  const hostURI =
-    process.env.NODE_ENV !== "production"
-      ? `${process.env.DEV_HOST_URI}`
-      : `${process.env.HOST_URI}`;
-
-  switch (type) {
-    case "welcome":
-      html = welcomeTemplate(userName);
-      subject = res.__("Welcome_to_the_platform") + " " + userName;
-      break;
-
-    case "resetPassword":
-      if (!email) {
-        return res.status(400).json({
-          state: "error",
-          message: res.__("Please_enter_a_valid_email"),
-        });
-      }
-
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        return res.status(404).json({
-          state: "error",
-          message: res.__("email_not_found", { email }),
-        });
-      }
-
-      const token = crypto.randomBytes(20).toString("hex");
-
-      user.resetPasswordToken = token;
-      user.resetPasswordExpires = Date.now() + 3600000;
-
-      await user.save();
-
-      const resetLink = `${hostURI}/reset-password/${token}`;
-      subject = res.__("Reset_your_password") + " " + user.username;
-      html = resetPasswordTemplate(user.username, resetLink, res);
-      break;
-
-    default:
-      return res.status(400).json({ message: res.__("Invalid_email_type") });
-  }
-
-  // Enviar la solicitud al microservicio de correos
-  emailRequester.send({ type: 'create-mail', email, subject, html }, (err, response) => {
-    
-    if (err) {
-      return res.status(500).json({ message: res.__("Email_not_sent") });
-    }
-
-    res.status(200).json({
-      state: "success",
-      message: res.__("Email_sent_successfully"),
-    });
   });
 });
--- src/mailing-service/mailingService.js
+++ src/mailing-service/mailingService.js
@@ -1,39 +1,21 @@
 'use strict';
 const { Responder } = require('cote');
-const nodemailer = require('nodemailer');
-const { welcomeTemplate, resetPasswordTemplate } = require('../utils/mainTemplates');
+const { transporter } = require('../utils/nodeMailer'); 
 
-const responder = new Responder({ name: 'create-mailing-service', key: 'email' });
-// Configuración del transporte de nodemailer
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "icraftyoumaster@gmail.com",
-        pass: "kaqy wxsm culf fhaw", 
-    },
-});
+const responder = new Responder({ name: 'nodemailer', key: 'email' });
 
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("Error in transporter configuration: ", error);
-    } else {
-        console.log("Server is ready to send emails");
-    }
-});
+
 responder.on('create-mail', async (req, done) => {
   const { email, subject, html } = req;
-console.log('microservice', email, subject, html);
   try {
-    // Enviar correo electrónico
+
     const sendedMail = await transporter.sendMail({
       from: 'ICraftYouMaster@gmail.com',
       to: email,
       subject: subject,
       html: html,
     });
-
+    console.log('Email enviado:', subject);
     if (!sendedMail) {
       return done({ message: 'Email_not_sent' });
     }
